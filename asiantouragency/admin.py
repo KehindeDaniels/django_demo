eddaf4213de6a97969ec03c044f0f2d6a6f4c6cb
kehindedaniels <kehindedaniels501@gmail.com>
2026-04-13 18:40:59 +0100

    Refactor admin.py to improve list_display comment and remove unused imports in views.py

--- asiantouragency/admin.py
+++ asiantouragency/admin.py
@@ -3,6 +3,7 @@ from .models import Tour
 # Register your models here.
 
 class TourAdmin(admin.ModelAdmin):
-    list_display = [f'{field.name}' for field in Tour._meta.fields]
+    # for each filed in the Tour model, return a list of field names
+    list_display = [field.name for field in Tour._meta.fields]
 
 admin.site.register(Tour, TourAdmin)
--- asiantouragency/views.py
+++ asiantouragency/views.py
@@ -1,6 +1,3 @@
-from venv import logger
-
-from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Tour
 
