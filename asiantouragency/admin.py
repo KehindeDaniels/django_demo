0e6525cd318877de6c8c196cd56764e7fce1b7c0
kehindedaniels <kehindedaniels501@gmail.com>
2026-04-13 18:18:08 +0100

    Register Tour model in admin with dynamic list display

--- asiantouragency/admin.py
+++ asiantouragency/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Tour
 # Register your models here.
+
+class TourAdmin(admin.ModelAdmin):
+    list_display = [f'{field.name}' for field in Tour._meta.fields]
+
+admin.site.register(Tour, TourAdmin)
