1873dbc527c107f72e39b66dec544d29138dfc6a
kehindedaniels <kehindedaniels501@gmail.com>
2026-04-18 00:17:58 +0100

    Add Post app with initial setup including models, views, and templates

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'asiantouragency.apps.AsiantouragencyConfig',
+    'post.apps.PostConfig',
 ]
 
 MIDDLEWARE = [
